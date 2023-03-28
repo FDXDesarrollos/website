f7cfc05d3d117d60d381cc53df66ce11e5e1f1f4
Frederick Juárez Velasco <70455496+FDXDesarrollos@users.noreply.github.com>
2023-03-28 15:49:51 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,127 @@
+<?php require_once("header.php"); ?>
+
+	<!-- Carrousel -->
+	<header class="carrusel">
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner" role="listbox">
+				<!-- Slide One - Set the background image for this slide in the line below -->
+				<div class="carousel-item active" style="background-image: url('img/slide-1.jpg')">
+					<div class="carousel-caption d-none d-md-block">
+						<h3>Paginas&nbsp;&bull;&nbsp;Sitios&nbsp;&bull;&nbsp;Sistemas Web</h3>
+					</div>
+				</div>
+				<!-- Slide Two - Set the background image for this slide in the line below -->
+				<div class="carousel-item" style="background-image: url('img/slide-2.jpg')">
+					<div class="carousel-caption d-none d-md-block">
+						<h3>Dise&ntilde;o de bases de datos</h3>
+					</div>
+				</div>
+				<!-- Slide Three - Set the background image for this slide in the line below -->
+				<div class="carousel-item" style="background-image: url('img/slide-3.jpg')">
+					<div class="carousel-caption d-none d-md-block">
+						<h3>Desarrollo de software</h3>
+					</div>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</header>
+	<!-- end Carrousel -->
+
+	<!-- Page Content -->
+	<div class="container">
+
+		<!-- About Me Section -->
+		<br/><br/>
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="text-center">Bienvenid@ a mi portafolio digital</h1>
+			</div>
+		</div>
+		<br/><br/>
+
+		<div class="row">
+			<div class="col-lg-4">
+                <p><img src="img/me.jpg" alt="Federico Juarez" class="img-responsive center-block img-circle grises"></p>
+                <p><img src="img/lenguajes.jpg" alt="Lenguajes Web 2.0" class="img-responsive center-block img-circle desaparece" style="position:absolute; top: 0px;"></p>
+			</div>		    
+			<div class="col-lg-8">
+				<h2 class="heading">Acerca de mi</h2>
+				<p class="lead">Me dedico a crear p&aacute;ginas, sitios, portales, sistemas y dem&aacute;s aplicaciones basadas en Web y Desktop.</p>
+				<p>Soy un desarrollador en tecnolog&iacute;as Web Back-end con Java, PHP/MySQL y Front-end con jQuery, HTML5 & CSS3, cuento con m&aacute;s de 7 a&ntilde;os de experiencia, trabajo y resido en la zona conurbada oriente de la CDMX.</p>
+				<p>Me apasiona el desarrollo de software, pues me gusta poder ofrecer nuevas perspectivas, impulsar mejoras y automatizar procesos adecuadamente. </p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<p>Visita mi <a href="https://fdxdesarrollo5.000webhostapp.com/portafolio.php">portafolio digital</a> en el cual encontraras varios proyectos Web desarrollados con PHP/MySQL, jQuery/Ajax y HTML5/CSS3 usando el patr&oacute;n de dise&ntilde;o web MVC con el framework CodeIgniter y algunas otras cosas m&aacute;s.</p>
+				<p>Puedes visitar mi perfil en <a href="https://www.linkedin.com/pub/federico-juárez/b6/313/833">LinkedIn</a> para conocer mi experiencia laboral &oacute <a href="https://fdxdesarrollo5.000webhostapp.com/contacto.php">haz click aqui</a> si deseas que trabajemos en algún proyecto juntos.</p>
+		 	</div>
+		</div>
+		<div class="row mb-4">
+			<div class="col-md-8">
+				<p>Tambi&eacute;n puedes descargar mi Curriculum Vitae en formato pdf para conocer m&aacute;s de mi.</p>
+			</div>
+			<div class="col-md-4">
+				<a class="btn btn-lg btn-success btn-block" href="javascript:void(0);" onClick="window.open ('https://fdxdesarrollo5.000webhostapp.com/cv/cvitae.php');">Descargar mi CV</a> <!-- cv/download.php' -->
+			</div>
+		</div>
+		<br/><br/>
+		<!-- end About Me -->
+
+		<hr><br/>
+
+		<!-- Features Section -->
+		<div class="row">
+			<div class="col-lg-6">
+				<h2>Ventajas de las Aplicaciones Web 2.0</h2>
+			  	<p>Una Aplicaci&oacute;n Web es un programa que est&aacute; accesible a trav&eacute;s de Internet.</p>
+				<ul>
+					<li>Soporta m&uacute;ltiples usuarios.</li>
+					<li>Las conexiones son seguras, como en las p&aacute;ginas web de los bancos.</li>
+					<li>Tambi&eacute;n es posible acceder a la informaci&oacute;n a trav&eacute;s de su smartphone, tablet, Notebook etc.</li>
+					<li>En el servidor s&oacute;lo hay un programa y puede ser usado por todos los empleados de una empresa. De esta forma, si se actualiza el programa, inmediatamente todos los empleados podr&aacute;n usar la nueva versi&oacute;n sin tener que instalar nada en sus computadoras.</li>
+					<li>No requiere instalar la Aplicaci&oacute;n Web en su ordenador porque ya est&aacute; en Internet. Puede tirar un ordenador, coger otro, conectarse a Internet y continuar trabajando.</li>
+					<li>Un largo etc. !!!</li>
+				</ul>
+			</div>
+			<div class="col-lg-6">
+				<a href="https://www.segundamano.mx/anuncios/ciudad-de-mexico/gustavo-a-madero/diseno-grafico-y-digital/programador-web-y-desktop-919264305">
+					<img class="img-fluid rounded" src="img/code-flat.jpg" alt="Aplicaciones Web 2.0">
+				</a>
+			</div>
+		</div>
+		<!-- end Features -->
+
+		<hr>
+
+		<!-- snippets Section -->
+		<div class="col-md-12 text-center">
+            <ul class="social-network social-circle">
+                <li><a href="https://github.com/FDXDesarrollos" class="icoLinkedin" title="GitHub"><i class="fa fa-github"></i></a></li>
+                <li><a href="https://www.facebook.com/frederick.juarezvelasco" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="https://twitter.com/" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="https://www.google.com.mx/" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="https://www.linkedin.com/pub/federico-juárez/b6/313/833" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+		</div>
+		<!-- end snippets -->
+
+		<hr>
+
+	</div>
+	<!-- end container -->
+
+<?php require_once("footer.php"); ?>
